113316c73b7243a1107966db8f4037a57b9eb2d1
Ruşen Özkal EMİROĞLU <56685102+RusenOzkalEmiroglu@users.noreply.github.com>
2025-12-18 13:19:08 +0300

    fix: Remove webpack config to resolve Vercel error

--- kredi/next.config.js
+++ kredi/next.config.js
@@ -17,16 +17,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer, webpack }) => {
-    if (isServer && config.name !== 'edge-server') {
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          '__dirname': JSON.stringify(process.cwd()),
-        })
-      );
-    }
-    return config;
-  },
 
 }
 
