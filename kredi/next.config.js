ebb37a3c3ceed1330c38bf348b30ed58fb5a2a63
Ruşen Özkal EMİROĞLU <56685102+RusenOzkalEmiroglu@users.noreply.github.com>
2026-01-14 18:02:34 +0300

    chore: reset next.config.js to default to test runtime error

--- kredi/next.config.js
+++ kredi/next.config.js
@@ -1,26 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['cheerio'],
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/cheerio',
-        'node_modules/cheerio/**/*',
-      ],
-    },
-  },
-  webpack: (config, { webpack, isServer }) => {
-    if (isServer) {
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          'global.GENTLY': 'false',
-          __dirname: '""',
-          __filename: '""',
-        })
-      );
-    }
-    return config;
-  },
   images: {
     domains: ['gmqfjtgbjprhxbxbhhms.supabase.co', 'uxtlcbcnwmxeyszhlewf.supabase.co'],
     remotePatterns: [
